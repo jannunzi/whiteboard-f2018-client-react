cf0b14fd663eec222abdd60b0d9e5db85b3bb135
jannunzi <jannunzi@gmail.com>
2018-10-17 18:42:13 -0400

    select user

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -4,9 +4,14 @@ export default class UserList extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      users: []
+      users: [],
+      selectedUser: {}
     }
   }
+  selectUser = user =>
+    this.setState({
+      selectedUser: user
+    })
   componentDidMount() {
     fetch("http://localhost:8080/api/user")
       .then(response => response.json())
@@ -16,15 +21,19 @@ export default class UserList extends Component {
   }
   render() {
     return(
-      <ul>
-        {
-          this.state.users.map((user, idx) =>
-            <li key={idx}>
-              {user.username}
-            </li>
-          )
-        }
-      </ul>
+      <div>
+        <h2>Users</h2>
+        <ul>
+          {
+            this.state.users.map((user, idx) =>
+              <li onClick={() => this.selectUser(user)} key={idx}>
+                {user.username}
+              </li>
+            )
+          }
+        </ul>
+        <h3>{this.state.selectedUser ? this.state.selectedUser.username:''}</h3>
+      </div>
     )
   }
 }
