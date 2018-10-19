9bb0e6c001f348bc2b22afd444beb4676a56edd5
jannunzi <jannunzi@gmail.com>
2018-10-18 21:20:09 -0400

    added lesson list

---
+++ src/components/LessonList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+export default class LessonList extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return(
+      <div>
+        <h2>Lessons</h2>
+      </div>
+    )
+  }
+}
